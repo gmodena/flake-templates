0a74ae21a5c42acc505f87cd2f4a2449ddf0bed9
Gabriele Modena <gmodena@pm.me>
2022-12-02 17:01:19 +0100

    Add top level flake.nix

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "A collection of flake templates";
+
+  outputs = { self, ... }: {
+    templates = {
+      pyspark-legacy-base-dev-env = {
+        path = ./pyspark-legacy-base-dev-env;
+        description = "jdk8, python3.7 and pyspark development environment for macOS";
+      };
+    };
+  };
+}
